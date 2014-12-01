b283fed592b9faf2202f0d6e7572e554465c5efb
Julian K. Arni <jkarni@gmail.com>
2014-12-01 14:55:43 +0100

    Proxy API.
            Start doing the drop-in replacement API.

--- Data/Cretheus.hs
+++ Data/Cretheus.hs
@@ -4,9 +4,25 @@ module Data.Cretheus where
 import Control.Monad
 import Control.Applicative
 import Data.Monoid
-import Data.Aeson
+import qualified Data.Aeson as A
 import Data.Aeson.Types
 import Data.Text
+
+-- Compat
+
+(.:) ::  FromJSON a => Object -> Text -> ReifiedP a
+(.:)  = (:.:)
+
+(.:?) :: FromJSON b => Object -> Text -> ReifiedP (Maybe b)
+(.:?) = (:.:?)
+
+(.!=) :: ReifiedP (Maybe a) -> a -> ReifiedP a
+(.!=) = (:.!=)
+
+
+class FromJSON a where
+    parseJSON :: Value -> ReifiedP a
+
 
 data ReifiedP a where
     (:.:)  :: FromJSON a => Object -> Text -> ReifiedP a
@@ -41,11 +57,10 @@ instance Monoid (ReifiedP a) where
     mempty = mzero
     mappend = mplus
 
-
 interpretR :: ReifiedP a -> Parser a
-interpretR (a :.:  b) = a .: b
-interpretR (a :.:? b) = a .:? b
-interpretR (a :.!= b) = interpretR a .!= b
+interpretR (a :.:  b) = a A..: b
+interpretR (a :.:? b) = a A..:? b
+interpretR (a :.!= b) = interpretR a A..!= b
 interpretR (a :>>= f) = interpretR a >>= interpretR . f
 interpretR (Ret a)     = return a
 interpretR Mzero       = mzero
