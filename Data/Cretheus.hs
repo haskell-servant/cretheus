1fd5df7b85d725806dc4bd37382a5ef53159caba
Julian K. Arni <jkarni@gmail.com>
2014-12-02 09:57:24 +0100

    Working proof of concept.

--- Data/Cretheus.hs
+++ Data/Cretheus.hs
@@ -1,17 +1,25 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Data.Cretheus where
 
 import Control.Monad
 import Control.Applicative
-import Data.Monoid
 import qualified Data.Aeson as A
 import qualified Data.Aeson.Types as A
+import Data.Data
+import Data.Foldable
+import Data.Monoid
 import Data.Text
+import Data.Typeable
+import Data.Scientific
 import Data.Proxy
 
 -- Schema
@@ -30,8 +38,8 @@ data Schema' where
     Or  :: Schema' -> Schema' -> Schema'
 
 mkSchema :: Show a => ReifiedP a -> Schema'
-mkSchema (a :.:  txt) = Required txt $ typeOf (Proxy::Proxy a)
-mkSchema (a :.:? txt) = Optional txt $ typeOf (Proxy::Proxy a)
+mkSchema (a :.:  txt) = Required txt $ typeOf' (Proxy::Proxy a)
+mkSchema (a :.:? txt) = Optional txt $ typeOf' (Proxy::Proxy a)
 mkSchema (a :.!= def) = case mkSchema a of
                             Optional txt typ -> Default txt (show def) typ
 {-mkSchema (a :>>= f) = m-}
@@ -40,8 +48,8 @@ mkSchema (a :.!= def) = case mkSchema a of
 mkSchema (Mplus a b) = mkSchema a `And` mkSchema b
 
 
-typeOf :: Proxy a -> Typ
-typeOf = undefined
+typeOf' :: Proxy a -> Typ
+typeOf' = undefined
 -- Compat
 
 (.:) ::  FromJSON a => A.Object -> Text -> ReifiedP a
@@ -53,16 +61,42 @@ typeOf = undefined
 (.!=) :: Show a => ReifiedP (Maybe a) -> a -> ReifiedP a
 (.!=) = (:.!=)
 
+-- Non-strict version of 'A.Value'
+data Value = Object A.Object
+           | Array A.Array
+           | String Text
+           | Number Scientific
+           | Bool Bool
+           | Null
+             deriving (Eq, Show, Typeable, Data)
+
+interpretV :: Value -> A.Value
+interpretV (Object !v) = A.Object v
+interpretV (Array !v)  = A.Array v
+interpretV (String !v) = A.String v
+interpretV (Number !v) = A.Number v
+interpretV (Bool !v)   = A.Bool v
+interpretV Null        = A.Null
+
+interpretV' :: A.Value -> Value
+interpretV' (A.Object v) = Object v
+interpretV' (A.Array v)  = Array v
+interpretV' (A.String v) = String v
+interpretV' (A.Number v) = Number v
+interpretV' (A.Bool v)   = Bool v
+interpretV' A.Null       = Null
 
 class FromJSON a where
-    parseJSON :: A.Value -> ReifiedP a
+    parseJSON :: Value -> ReifiedP a
 
 instance FromJSON a => A.FromJSON a where
-    parseJSON = interpretR . parseJSON
+    parseJSON = interpretR . parseJSON . interpretV'
 
 
 -- It'll be important to keep track of which fields may be evaluated, and
--- which may not.
+-- which may not. In essence, we want all arguments that may be coming
+-- from inside the outer constructor of the `Value` passed to `parseJSON`
+-- to not be used - e.g., the first argument of `(:.:)`.
 data ReifiedP a where
     (:.:)  :: FromJSON a => A.Object -> Text -> ReifiedP a
     (:.:?) :: FromJSON b => A.Object -> Text -> ReifiedP (Maybe b)
@@ -71,7 +105,13 @@ data ReifiedP a where
     Ret    :: a -> ReifiedP a
     Mzero  :: ReifiedP a
     Mplus  :: ReifiedP a -> ReifiedP a -> ReifiedP a
-    WithText :: String -> (Text -> ReifiedP a) -> A.Value -> ReifiedP a
+    WithText :: String -> (Text -> ReifiedP a) -> Value -> ReifiedP a
+
+{-
+- (:<*>) === RP (a -> b) -> RP a -> RP b
+-   \---> +/-  AND
+- <*> = m1 >>= \x1 -> m2 >>= \x2 -> return $ x1 x2
+-}
 
 
 instance Monad ReifiedP where
@@ -97,6 +137,9 @@ instance Monoid (ReifiedP a) where
     mempty = mzero
     mappend = mplus
 
+-- | Convert a ReifiedP to a Parser.
+-- Currently this is used to interface with all 'aeson' functions, but
+-- that suffers from a (probably avoidable) performance penalty.
 interpretR :: ReifiedP a -> A.Parser a
 interpretR (a :.:  b) = a A..: b
 interpretR (a :.:? b) = a A..:? b
@@ -105,7 +148,7 @@ interpretR (a :>>= f) = interpretR a >>= interpretR . f
 interpretR (Ret a)     = return a
 interpretR Mzero       = mzero
 interpretR (Mplus a b) = mplus (interpretR a) (interpretR b)
-interpretR (WithText a f v) = A.withText a (interpretR . f) v
+interpretR (WithText a f v) = A.withText a (interpretR . f) (interpretV v)
 
 
 ----Test------------------------------------------------------------------
@@ -121,10 +164,25 @@ t1 :: Maybe Test1
 t1 = A.decode "{\"field1\": \"Field 1\", \"field2\": \"Field 2\"}"
 
 t1' :: String
-t1' = show' (parseJSON obj::ReifiedP Test1)
-    where obj = A.object [ ("field1", A.String "")
-                       , ("field2", A.String "")
-                       ]
+t1' = show $ show' <$> (toList $ fillOut undefined::[ReifiedP Test1])
+
+data Tree f a = Node a
+              | Tree (f (Tree f a))
+              deriving (Foldable, Functor)
+
+-- | Pass values with all possible outermost constructors to 'parseJSON',
+-- and collect the results.
+--
+fillOut :: FromJSON a => a -> Tree [] (ReifiedP a)
+fillOut _ = Tree
+               [ Node $ parseJSON (Array u)
+               , Node $ parseJSON (Object u)
+               , Node $ parseJSON (String u)
+               , Node $ parseJSON (Number u)
+               , Node $ parseJSON (Bool u)
+               , Node $ parseJSON Null
+               ]
+    where u = error "I wasn't meant to be evaluated!"
 
 show' :: ReifiedP a -> String
 show' (a :.:  b) = "Required: " ++ unpack b
@@ -137,6 +195,6 @@ show' (Mplus a b) = show' a ++ "or\n" ++ show' b
 show' (WithText a f v) = "Expects: " ++ a  ++ show' (f undefined)
 
 instance FromJSON Test1 where
-    parseJSON (A.Object v) = Test1 <$> v .: "field1"
+    parseJSON (Object v) = Test1 <$> v .: "field1"
                                    <*> v .: "field2"
     parseJSON _            = mzero
